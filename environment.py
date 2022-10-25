dd81e045b2d38c0bd245ea3b2716dccd6935be99
PrestonComer <1123preston1123@gmail.com>
2022-10-25 14:35:30 -0500

    finished env, created robot that cleans randomly

--- environment.py
+++ environment.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 DIRECTIONS = {
     "Up" : (-1, 0),
@@ -7,10 +7,10 @@ DIRECTIONS = {
     "Left" : (0, -1)
 }
 
-POSSIBLEFINDS = ["Empty", "Robot", "Dirt", "Object"]
+POSSIBLEFINDS = ["Clean", "Robot", "Dirt", "Object"]
 
 class environment:
-    def __init__(self, numRows, numCols, robotLocation, cleanLocations=[], 
+    def __init__(self, numRows, numCols, robotLocation=(0,0), cleanLocations=[], 
                 objectLocations=[]):
         self.numRows = numRows
         self.numCols = numCols
@@ -34,16 +34,18 @@ class environment:
         # If you fail to move the robot 10 times then give an error asking the
         # user to please try and enter a better starting location
         if self.env[self.robotLocation[0]][self.robotLocation[1]] == "Object":
-            randRow = random.randint(0, numRows - 1)
-            randCol = random.randint(0, numCols - 1)
+            randRow = randint(0, numRows - 1)
+            randCol = randint(0, numCols - 1)
             unluckyRobot = 0
             while self.env[randRow][randCol] == "Object" and unluckyRobot < 10:
                 unluckyRobot += 1
-                randRow = random.randint(0, numRows - 1)
-                randCol = random.randint(0, numCols - 1)
+                randRow = randint(0, numRows - 1)
+                randCol = randint(0, numCols - 1)
             if unluckyRobot >= 10:
                 raise ValueError("Please Pick A Valid Robot Starting Location!")
             self.env[randRow][randCol] = "Robot"
+        else:
+            self.env[robotLocation[0]][robotLocation[1]] = "Robot"
 
     def setLocation(self, location, find):
         self.env[location[0]][location[1]] = find
@@ -52,11 +54,43 @@ class environment:
     # True means that it is, False means that it is not
     def inBounds(self, location):
         row, col = location
-        if row <= 0 and col <= 0:
+        if row < 0 or col < 0:
             return False
-        if row >= self.numRows and col >= self.numCols:
+        if row >= self.numRows or col >= self.numCols:
             return False
         return True
 
+    # Given a location check to see what is at that location. Assumes that the 
+    # location given is in bounds of the environment
+    def checkLocation(self, location):
+        return self.env[location[0]][location[1]]
+
+    # Print the environment where (0, 0) is in the top left
+    def displayEnvironment(self):
+        print()
+        for row in self.env:
+            print(row)
+
+    def allClean(self):
+        for row in self.env:
+            if row.count("Dirt") != 0:
+                return False
+        return True
+
     def moveRobot(self, direction):
-        pass
+        if not direction in DIRECTIONS:
+            raise ValueError("Improper Direction Entered")
+
+        (robotLocRow, robotLocCol) = self.robotLocation
+        (movementRow, movementCol) = DIRECTIONS[direction]
+        newLocation = (robotLocRow + movementRow, robotLocCol + movementCol)
+
+        # the place the robot is moving has to be inbounds and not blocked
+        if not self.inBounds(newLocation):
+            return
+        if self.checkLocation(newLocation) == "Object":
+            return
+
+        self.env[self.robotLocation[0]][self.robotLocation[1]] = "Clean"
+        self.robotLocation = newLocation
+        self.env[newLocation[0]][newLocation[1]] = "Robot"
---
+++ randomRobot.py
@@ -0,0 +1,40 @@
+from environment import *
+from random import choice
+
+class randomRobot:
+    def __init__(self, environment):
+        self.env = environment
+        self.totalMoves = 0
+        self.moveCounters = {
+            "Up" : 0,
+            "Right" : 0,
+            "Down" : 0,
+            "Left" :0
+        }
+
+    def moveInformation(self, shouldPrint=False):
+        if shouldPrint:
+            print(f"\nTotal Moves: {self.totalMoves}")
+            print(f"Moves Up: {self.moveCounters['Up']}")
+            print(f"Moves Right: {self.moveCounters['Right']}")
+            print(f"Moves Down: {self.moveCounters['Down']}")
+            print(f"Moves Left: {self.moveCounters['Left']}")
+        return (self.totalMoves, self.moveCounters)
+
+    def cleanEnvironment(self):
+        print("\nStarting Environment:", end="")
+        self.env.displayEnvironment()
+
+        while not self.env.allClean():
+            moveChoice = choice(list(DIRECTIONS))
+
+            self.moveCounters[moveChoice] += 1
+            self.totalMoves += 1
+
+            self.env.moveRobot(moveChoice)
+            print("\nMove " + str(self.totalMoves) + " - " + moveChoice + ":", end="")
+            self.env.displayEnvironment()
+
+        print("\nEnvironment All Clean!", end="")
+        self.env.displayEnvironment()
+        self.moveInformation(True)
